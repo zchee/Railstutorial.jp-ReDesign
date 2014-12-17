d90bd29964d8811bd904046dfde34400037536d7
zchee <zchee@zsh.us>
2014-12-18 04:29:27 +0900

    Refactoring

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,13 +1,13 @@
+'use strict';
 // Called when the url of a tab changes.
 function checkMatchesUrl(tabId, changeInfo, tab) {
-    'use strict';
-    var manifest = chrome.runtime.getManifest();
-    var matchesUrl = manifest.content_scripts[0].matches[0].slice(0, -1);
-        console.log("checkForValidUrl");
-    if (tab.url.indexOf(matchesUrl) > -1) {
-        // ... show the page action.
-        chrome.pageAction.show(tabId);
-    }
+  var manifest = chrome.runtime.getManifest();
+  var matchesUrl = manifest.content_scripts[0].matches[0].slice(0, -1);
+  console.log("checkForValidUrl");
+  if (tab.url.indexOf(matchesUrl) > -1) {
+    // ... show the page action.
+    chrome.pageAction.show(tabId);
+  }
 }
 
 // Listen for any changes to the URL of any tab.
